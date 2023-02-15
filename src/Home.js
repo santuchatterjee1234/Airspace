032e251296041467381b76b1b92112900ce9752d
Santu <santuchatterjee123@gmail.com>
2023-02-15 17:14:30 +0530

    second

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ render(){
                     <div class="row">
                         <div class="col-md-12">
                             <div class="block">
-                                <h1 class="animated fadeInUp">A Digital Marketing <br/> Design Agency</h1>
+                                <h1 class="animated fadeInUp">Digital Marketing <br/> Design Agency</h1>
                                 <p class="animated fadeInUp">We love the Web and the work we do.We work closely with our clients to deliver <br/>  the best possible solutions for their needs</p>
                                 <a href=""  class="btn btn-main animated fadeInUp" >Airspace</a>
                             </div>
